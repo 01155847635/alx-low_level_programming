4a0a6e0e1bc5bd139560a54e4961cac5b3867bfd
Abdelrahman Ali Saad <67483743+01155847635@users.noreply.github.com>
2023-02-17 23:07:25 +0200

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char low;
+	char upper;
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+    if(low != 'e' or low != 'q') 
+    {
+		putchar(low);
+    }
+	}
+	putchar('\n');
+	return (0);
+}
