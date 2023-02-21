524a759a30dd38b434c24761bd42c342f9548f6c
Abdelrahman Ali Saad <67483743+01155847635@users.noreply.github.com>
2023-02-21 23:44:41 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include "main.h"
 
 int main(void)
 {
   string word = "_putchar";
   int i;
   for (i = 0; i < 9; i++)
-    putchar(word[i]);
-	putchar('\n');
+    _putchar(word[i]);
+	_putchar('\n');
 	return (0);
 }
