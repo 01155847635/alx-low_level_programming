d47fc3852824a583540d232153af4ac7de924cfc
Abdelrahman Ali Saad <67483743+01155847635@users.noreply.github.com>
2023-02-22 14:20:43 +0200

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,35 +8,35 @@
 void print_times_table(int n)
 {
 	int rone, cone, d;
-
-	for (rone = 0; rone <= n; rone++)
+	if(n<15 && n>0)
 	{
-		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
-		for (cone = 1; cone <= n; cone++)
+		
+		for (rone = 0; rone <= n; rone++)
 		{
-      if((rone < 15 && rone > 0) && (cone < 15 && cone > 0))
-      { 
-			  d = (rone * cone);
-			  if ((d / 10) > 0)
-			  {
-			  	_putchar((d / 10) + '0');
-			  }
-			  else
-			  {
-				  _putchar(' ');
-			  }
-			  _putchar((d % 10) + '0');
+			_putchar('0');
+			_putchar(',');
+			_putchar(' ');
+			for (cone = 1; cone <= n; cone++)
+			{
+				d = (rone * cone);
+				if ((d / 10) > 0)
+				{
+					_putchar((d / 10) + '0');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+				_putchar((d % 10) + '0');
 
-			  if (cone < 9)
-			  {
-				  _putchar(',');
-				  _putchar(' ');
-			  }
-		  _putchar('\n');
-      }
-    }
+				if (cone < n)
+				{
+				_putchar(',');
+				_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
 	}
 
 }
