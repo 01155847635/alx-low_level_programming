a45cb6c4b88848944693a631a876407776bb6a8e
Abdelrahman Ali Saad <67483743+01155847635@users.noreply.github.com>
2023-02-28 00:09:59 +0200

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,9 +7,16 @@
  */
 void puts2(char *str)
 {
-	int i;
-	
-	for (i = 0; str[i] != '\0'; i += 2)
+	int len, i;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i += 2)
 	{
 		_putchar(str[i]);
 	}
