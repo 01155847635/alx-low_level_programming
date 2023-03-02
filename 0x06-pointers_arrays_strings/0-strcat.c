1547db74fca0c30e95132216d2063365c3fbd63c
Abdelrahman Ali Saad <67483743+01155847635@users.noreply.github.com>
2023-03-03 00:15:54 +0200

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,14 +1,24 @@
-#include 'main.h'
+#include "main.h"
+
+/**
+ *_strcat - Write a function that concatenates two strings.
+ *
+ *@dest: This is the output dest
+ *@src: This is the input source
+ *
+ * Return: This return to dest, that concatenates two strings
+ */
 
 char *_strcat(char *dest, char *src)
 {
-  int len1=0,len2=0;
-  char *temp=0;
-  for(int i=0;dest[i]!='\0';i++)len1++;
-  for(int i=0;src[i]!='\0';i++)len2++;
-  temp = new char[len1+len2+100];
-  strcpy(temp,dest);
-  strcat(temp,src);
-  dest = temp;
-  return dest;
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	return (dest);
 }
